11a329131d1c4361da9d3b1da1e1c4f1c391bc9b
bjjartem <81825483+bjjartem@users.noreply.github.com>
2021-04-02 20:52:45 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "Header.h"
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	/*{
+		string rus;
+		string eng;
+		int Word(string r, string e)
+		{
+			rus = r; eng = e;
+		}
+
+		WORD() {}
+	}
+
+	WORD dict[20];
+	dict[0].setWord("Привет", "Hello");
+	dict[1].setWord("Мама", "Mother");
+	/*setlocale(LC_ALL, "rus");
+	for (int i = 0; i < 20; i++)
+	{
+		for (int j = 0; j < 20; j++)
+		{
+
+		}
+	}*/
+
+
+	string rus[1] = { "Привет" };
+	string eng[1] = { "Hello" };
+
+
+}
